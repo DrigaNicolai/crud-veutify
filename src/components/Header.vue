ddbf23a84cf343a165f4da9822333b9e3aa396b1
DrigaNicolai <just.whyme7@gmail.com>
2022-02-11 18:59:42 +0200

    Alpha version of crud-vue

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar
+    color="green"
+    dark
+    app
+  >
+    <v-col class="d-flex">
+      <v-app-bar-title>
+        <h2 class="text-h3">CRUD Vuetify</h2>
+      </v-app-bar-title>
+    </v-col>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+};
+</script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-row class="mx-2 mt-6 d-flex justify-center">
+    <v-col md=4 lg=8>
+      <h1 class="text-h2 mb-6">List of todos</h1>
+      <v-list
+        two-line
+      >
+        <v-list-item
+          v-for="todo in todos"
+          :key="todo.id"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ todo.title }}</v-list-item-title>
+            <v-list-item-subtitle>{{ todo.description }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          id: 0,
+          title: "Title 1",
+          description: "Description for the element with number 1"
+        },
+        {
+          id: 1,
+          title: "Title 2",
+          description: "Description for the element with number 2"
+        },
+        {
+          id: 2,
+          title: "Title 3",
+          description: "Description for the element with number 3"
+        },
+        {
+          id: 3,
+          title: "Title 4",
+          description: "Description for the element with number 4"
+        },
+        {
+          id: 4,
+          title: "Title 5",
+          description: "Description for the element with number 5"
+        }
+      ]
+    }
+  }
+}
+</script>
+<style>
+  
+</style>
